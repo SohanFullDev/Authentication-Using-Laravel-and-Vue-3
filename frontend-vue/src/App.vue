f1bcc52c05a0dd84034d9bfcfd509c9187881662
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-09 13:21:43 +0600

    2.5_bootstrap with vue_e

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 
-
+import Nav from './components/Nav.vue'
 </script>
 
 <template>
-  <div>
+    <Nav/>
+  <div class="m-3">
+
     <h3>Laravel & Vue Js Authentication</h3>
 
   </div>
