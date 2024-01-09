335809f70c215b70139ac882ab4b2f149c462f9f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-09 16:20:32 +0600

    3.3_loging & get user_c

--- frontend-vue/src/pages/Home.vue
+++ frontend-vue/src/pages/Home.vue
@@ -1,5 +1,22 @@
 <template>
-    <h1>Home Page</h1>
+    <div class="container">
+        <h1>Home Page</h1>
+        <h3>{{ user?.name}}</h3>
+        <h3>{{ user?.email}}</h3>
+    </div>
 </template>
 <script setup>
+ import { ref, onMounted} from 'vue';
+ import axios from 'axios'
+
+ const user = ref();
+
+ onMounted(async () =>{
+    const data = await axios.get('/api/user');
+   // console.log(data);
+    user.value = data.data
+
+ });
+
+
 </script>
