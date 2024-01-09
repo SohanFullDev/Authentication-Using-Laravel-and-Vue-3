106cef99a7e8c10d5103c5d1d5e580dfe4c910c2
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-09 15:51:30 +0600

    3.2_login & get user_b

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
+import '../axios';
 //createApp(App).mount('#app')
 const app = createApp(App);
 app.use(router)
--- frontend-vue/src/pages/Login.vue
+++ frontend-vue/src/pages/Login.vue
@@ -2,19 +2,20 @@
     <div class="container">
          <div class="col-md-6 m-auto">
             <h1>Login Page</h1>
-        <div>
+        <div class="mt-3">
+            <form @submit.prevent="submitLogin">
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" v-model="form.email">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleFormControlInput1">
+                <input type="password" class="form-control" id="exampleFormControlInput1" v-model="form.password">
             </div>
             <div>
                 <button type="submit" class="btn btn-primary">Login</button>
             </div>
-
+        </form>
 
         </div>
          </div>
@@ -22,4 +23,44 @@
     </div>
 </template>
 <script setup>
+import {ref } from 'vue';
+import axios from 'axios'
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+const form = ref({
+    email: '',
+    password: ''
+});
+
+
+const submitLogin = async () => {
+    await axios.post('/login',{
+        email: form.value.email,
+        password: form.value.password
+
+    });
+
+    router.push('/');
+}
+/*
+const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
+
+const submitLogin = async () => {
+    await axios.post('/login', {
+        email: form.value.email,
+        password: form.value.password
+    }, {
+        headers: {
+            'X-CSRF-TOKEN': csrfToken,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    router.push('/');
+}*/
+
+
 </script>
